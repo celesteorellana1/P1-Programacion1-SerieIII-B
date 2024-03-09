a85f3a8f5dbbc5bf4cd6c65c802edec6e0940619
celesteorellana1 <135485884+celesteorellana1@users.noreply.github.com>
2024-03-09 12:59:51 -0600

    Add files via upload

---
+++ PrimerParcialProgra1/PrimerParcialProgra1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PrimerParcialProgra1
+{
+    class Miprimerprograma
+    {
+        public static void Main(string[] args)
+        {
+            string correo;
+            string correoNuevo;
+
+            Console.WriteLine("Bienvenido a mi generador de correos");
+
+            Console.Write("Ingresa tu correo electronico  gmail: ");
+            correo = Console.ReadLine();
+
+            if (!string.IsNullOrWhiteSpace(correo) && correo.EndsWith("@gmail.com"))
+            {
+                correoNuevo = correo.Replace("@gmail.com", "@miumg.edu.gt");
+                Console.WriteLine($"Tu nuevo correo es: {correoNuevo}");
+            }
+            else
+            {
+                Console.WriteLine("El correo ingresado no es correcto");
+            }
+
+        }
+    }
+}
+
+
+
+
